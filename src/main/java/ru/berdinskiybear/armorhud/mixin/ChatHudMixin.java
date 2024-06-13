7f9c3acdd1b5759c32c4a088a1925d518962432f
poq <120293384+poqli@users.noreply.github.com>
2024-06-13 17:41:13 -0400

    New pushChatBox feature
    Implemented a mixin to push the chat HUD's history box
    Reordered the config screen

---
+++ src/main/java/ru/berdinskiybear/armorhud/mixin/ChatHudMixin.java
@@ -0,0 +1,85 @@
+package ru.berdinskiybear.armorhud.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import ru.berdinskiybear.armorhud.ArmorHudMod;
+import ru.berdinskiybear.armorhud.config.ArmorHudConfig;
+import java.util.ArrayList;
+import java.util.List;
+
+@Mixin(ChatHud.class)
+public class ChatHudMixin {
+    @Shadow @Final private MinecraftClient client;
+
+    @Unique
+    private int offset = 0;
+    @Unique
+    private final List<ItemStack> armorItems = new ArrayList<>(4);
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;getProfiler()Lnet/minecraft/util/profiler/Profiler;", shift = At.Shift.AFTER))
+    public void calculateOffset(DrawContext context, int currentTick, int mouseX, int mouseY, boolean focused, CallbackInfo ci) {
+        ArmorHudConfig config = this.getArmorHudConfig();
+        if (config.isEnabled() && config.isPushChatBox()) {
+            int add = 0;
+            if (config.getAnchor() == ArmorHudConfig.Anchor.Bottom && config.getSide() == ArmorHudConfig.Side.Left && config.getOrientation() == ArmorHudConfig.Orientation.Vertical) {
+                int amount = 0;
+                PlayerEntity playerEntity = this.getCameraPlayer();
+                if (playerEntity != null) {
+                    this.armorItems.clear();
+                    if (config.getSlotsShown() == ArmorHudConfig.SlotsShown.Always_Show)
+                        amount = 4;
+                    else {
+                        List<ItemStack> armorList = playerEntity.getInventory().armor;
+                        for (ItemStack itemStack : armorList) {
+                            if (!itemStack.isEmpty()) {
+                                amount++;
+                                if (config.getSlotsShown() != ArmorHudConfig.SlotsShown.Show_Equipped) {
+                                    amount = 4;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    if (amount != 0 && config.getOffsetY() > config.getMinOffsetBeforePushingChatBox())
+                        add += config.getOffsetY() - config.getMinOffsetBeforePushingChatBox();
+                    if (config.getOrientation() == ArmorHudConfig.Orientation.Vertical)
+                        add += 20 * (amount - 1);
+                }
+            }
+            this.offset = Math.max(add, 0);
+        } else
+            this.offset = 0;
+    }
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;translate(FFF)V", shift = At.Shift.AFTER, ordinal = 0))
+    public void offset(DrawContext context, int currentTick, int mouseX, int mouseY, boolean focused, CallbackInfo ci) {
+        context.getMatrices().translate(0.0F, -((float) this.offset), 0.0F);
+    }
+
+    /**
+     * This function determines which config is supposed to be current. Usually the loaded config is considered current
+     * but if config screen is open then the preview config is used as current.
+     *
+     * @return Current config
+     */
+    @Unique
+    private ArmorHudConfig getArmorHudConfig() {
+        return this.client.currentScreen != null && this.client.currentScreen.getTitle() == ArmorHudMod.CONFIG_SCREEN_NAME ? ArmorHudMod.previewConfig : ArmorHudMod.getConfig();
+    }
+
+    @Unique
+    private PlayerEntity getCameraPlayer() {
+        return !(this.client.getCameraEntity() instanceof PlayerEntity) ? null : (PlayerEntity) this.client.getCameraEntity();
+    }
+}
